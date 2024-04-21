68413f575c56c777b55b0d19ebe56213596bcc92
aney9 <isip_t.a.murashov@mpt.ru>
2024-04-21 16:15:41 +0300

    v modeli anekdot stavte 2

---
+++ Model/model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace krestikashotikDva.Model
+{
+    internal class model
+    {
+        public string Nickname { get; set; }
+        public string Password { get; set; }
+       
+        public model(string nickname, string password)
+        {
+            nickname = Nickname;
+            password = Password;
+        }
+    }
+}
---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using krestikashotikDva.View;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace krestikashotikDva
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void vhod_Click(object sender, RoutedEventArgs e)
+        {
+            igra igra = new igra();
+            igra.Show();
+            this.Close();
+        }
+
+        private void registr_Click(object sender, RoutedEventArgs e)
+        {
+            registration registr = new registration();
+            registr.Show();
+            this.Close();
+        }
+    }
+}
